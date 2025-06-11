4f4aa9ddb410e7a487ab46783c3585b3d7956f09
PIN YI <880831ian@gmail.com>
2025-06-12 01:33:33 +0800

    fix: 修正 Swagger 路徑

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	// 根據環境變數設定 host
 	if os.Getenv("ENV") == "production" {
 		docs.SwaggerInfo.Host = "api.pin-yi.me"
-		docs.SwaggerInfo.BasePath = "/taiwan-calendar"
+		docs.SwaggerInfo.BasePath = "/"
 		docs.SwaggerInfo.Schemes = []string{"https"}
 	} else {
 		docs.SwaggerInfo.Host = "127.0.0.1"
